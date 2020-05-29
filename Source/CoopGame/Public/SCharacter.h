ff9affab5ce5a6ff7b507f45d9c13c08624604e6
vsensu <vsensu@foxmail.com>
2020-05-29 15:11:04 +0800

    Third person camera part 2

--- Source/CoopGame/Public/SCharacter.h
+++ Source/CoopGame/Public/SCharacter.h
@@ -26,6 +26,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
 	class UCameraComponent* CameraComp;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class USpringArmComponent* SpringArmComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
